9f8c487dfb424b4e3d6a1901843cc8f7e13812b9
namgivu <namgivu@gmail.com>
2023-11-30 13:47:11 +0000

    sync code

---
+++ main.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+print(datetime.now() )
+
+###
+
+print('hi Nam')
+print ('hi')
+print("hi phuong")
+print('hi huynh')
+print('ok ban')
+print('hi Kasumo2009 -Hieu')
+
+print('Mai Manh Cuong')
+print('''
+chao tat ca moi nguoi
+hello world
+chao ban huynh
+''')
+
+###
+
+print(1)
+print(22)
+
+print('a')
+print('abb')
+print('abb ccc')
+
+print("a")
+print("abb")
+print("abb ccc")
+
+print('''
+abb line1
+ccc line2
+''')
+
+print("""
+abb line1
+ccc line2
+""")
+
+print('abb'
+      'ccc'
+      'deefff'
+     )
