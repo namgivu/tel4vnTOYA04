2149c3efe95101cd3216921c0832ea6384ea3e14
namgivu <namgivu@gmail.com>
2023-12-05 20:22:21 +0700

    ifelse emptystr

---
+++ s03_ifelse/eg2_emptystr_is_False/main.py
@@ -0,0 +1,7 @@
+# .venv/bin/python -m pyflowchart main.py -o ./pyflowchart/flowchart2.html
+
+if False: print(1)
+else:     print(0)
+
+if "": print(1)
+else:  print(0)
---
+++ s03_ifelse/s03_cau3_labw1.py
@@ -0,0 +1,18 @@
+print( "hello" == 'hello' )
+print( "hello" == 'Hello' )
+
+b = "hello" == 'hello'
+print(b)
+print(f'{b=}')
+
+print( (not True) and (not False) )
+print( (1<2) and (2>3) )
+print( (1<2) or  (2>3) )
+
+###
+
+if False: print(1)
+else:     print(0)
+
+if "": print(1)
+else:  print(0)
