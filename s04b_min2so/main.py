d9480943c7ed02e8e77cf231c2d72a9ef18f75eb
namgivu <namgivu@gmail.com>
2023-12-05 21:44:44 +0700

    some alternative min2so

--- s04b_min2so/main.py
+++ s04b_min2so/main.py
@@ -7,4 +7,12 @@ def min2so(a,b):
     i=122  # add code to force drawing diamond in flowchart
   return min
 
+def min2so_(a,b):
+  if a<b: return a
+  else:   return b
+
+def min2so__(a,b):
+  min = a if a<b else b
+  return min
+
 print( min2so(a=1,b=22) )
