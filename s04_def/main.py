0fc6846354cbed29a592f69a9e1b841b5274d1cd
namgivu <namgivu@gmail.com>
2023-12-05 21:09:37 +0700

    def return 0th

--- s04_def/main.py
+++ s04_def/main.py
@@ -8,8 +8,39 @@ def in_chaohoi():
 in_chaohoi()
 in_chaohoi()
 
+
 def chao(name):
   print('chao ' + name)
 
 chao(name='tel4vn')
 chao(name='toya04')
+
+
+#region convert_to_mb(gb=2)
+'''
+convert_to_mb(gb=2) -> 1024*2 = 2048
+convert_to_mb(gb=4) -> 1024*4 = 4096
+
+---
+SSD 1Tb
+RAM 4Gb
+baonhieu Mb?
+
+1Tb = 1024Gb
+         1Gb = 1024Mb 
+
+tudien == bit == codien/kocodien == 1/0  -> binary hecoso2
+1byte  ==8bit
+2^10 = 1024
+
+tan = 1000kg
+         1kg = 1000g
+'''
+def convert_to_mb(gb):
+  # return 'todo'
+  return None
+
+kq = convert_to_mb(gb=2)
+#endregion convert_to_mb(gb=2)
+
+debugend=''
