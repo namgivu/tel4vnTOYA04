7eaafa854b0dcce6139e53fb0874304c2f5b141c
namgivu <namgivu@gmail.com>
2023-12-07 20:38:24 +0700

    list + sum

---
+++ s07_list.py
@@ -0,0 +1,9 @@
+l = [1,22,333] ; print(l)
+print( [1,22,333] )
+print( [i for i in range(1, 10) ] )
+
+print()
+l      = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+chan_l = [i for i in l  if i%2==0 ] ; print(chan_l)
+
+float_l = [float(i) for i in l] ; print(float_l)
---
+++ s07_sum.py
@@ -0,0 +1,12 @@
+l = [1, 2, 3, 4, 5, 6]
+print( sum(l) )
+print( max(l) )
+print( min(l) )
+print( len(l) )
+
+print( f'{sum(l)=}' )
+print( f'{max(l)=}' )
+print( f'{min(l)=}' )
+print( f'{len(l)=}' )
+
+print( sorted(l, reverse=True) )
