7a0603c37664cd1aba05de8574ba84f156f49243
namgivu <namgivu@gmail.com>
2023-12-07 19:45:35 +0700

    loop for

---
+++ s05_loop/s05_loop_for.py
@@ -0,0 +1,15 @@
+for i in range(1,9):  # 1 2 ... 8
+  print(i)
+print('end for')
+
+for i in range(1,10): print(i)
+print('end for')
+
+for i in range(1,9+1): print(i)
+print('end for')
+
+###
+
+#   i      start,end, step
+for i in range(1,9+1, 2): print(i)
+print('end for')
