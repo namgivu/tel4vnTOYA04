9e2755611a01a4342e93425f11518dca5ec0611d
namgivu <namgivu@gmail.com>
2023-12-07 14:04:12 +0000

    sync

---
+++ s08_labw1_c6.py
@@ -0,0 +1,16 @@
+'''
+5. Write a function that can 
+calculate the sum of all even numbers from 1 to 10
+with “for” loop.
+
+6. Write the same function as above but with "while" loop.
+'''
+# by Hieu kasumo
+i=0
+s=0
+while(i<=10):
+  i = i +1
+  if (i %2 ==0): 
+    s = s + i
+print(s)
+  
