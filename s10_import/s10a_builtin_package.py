d19ce535986302d5fcb5a3c3857e80ebfc7693b2
namgivu <namgivu@gmail.com>
2023-12-12 20:08:22 +0700

    clearer code

--- s10_import/s10a_builtin_package.py
+++ s10_import/s10a_builtin_package.py
@@ -1,13 +1,13 @@
-'''
-get today date
-'''
+'''get today date'''
 #    package         module
 from datetime import date
 v = date.today() ; print(f'today is {v}')
 
+'''get now time'''
 from datetime import datetime
 print(f'now is {datetime.now()}')
 
+'''get timestamp'''
 d = datetime.now()
 print( datetime.strftime(d,                             '%Y%m%d_%H%M%S') )
 print( datetime.strftime(datetime(2000,1,22),           '%Y%m%d_%H%M%S') )
