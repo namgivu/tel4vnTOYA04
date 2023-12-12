1d324086bc2f567ae9f3a1466e8a4ccb17b61df2
namgivu <namgivu@gmail.com>
2023-12-12 20:34:38 +0700

    builtin package @ json sys os

--- s10_import/s10a_builtin_package.py
+++ s10_import/s10a_builtin_package.py
@@ -1,8 +1,12 @@
+import sys
+
+
 #region demo datetime
 '''get today date'''
 #    package         module
 from datetime import date
 v = date.today() ; print(f'today is {v}')
+# sys.exit()
 
 '''get now time'''
 from datetime import datetime
@@ -14,6 +18,7 @@ print( datetime.strftime(d,                             '%Y%m%d_%H%M%S') )
 print( datetime.strftime(datetime(2000,1,22),           '%Y%m%d_%H%M%S') )
 print( datetime.strftime(datetime(2008,9,10, 11,12,13), '%Y%m%d_%H%M%S') )
 #endregion demo datetime
+
 
 print()
 
@@ -42,3 +47,39 @@ l = [1,22,333]    ; print(f'before shuffle {l=}')
 random.shuffle(l) ; print(f'after  shuffle {l=}')
 
 #endregion demo random
+
+
+print()
+
+
+#region json
+import json
+d = {
+  'name': 'tel4vn',
+  'year': 2017,
+
+  'course': {
+    'toya': ['toya03','toya04'],
+    'toda': ['toda19','toda20','toda21'],
+  },
+}
+print(d)
+print(json.dumps(d, indent=2) )
+print(json.dumps(d, indent=4) )
+#endregion json
+
+
+#region sys
+exitnow=True
+exitnow=False
+if exitnow:
+  sys.exit()
+#endregion sys
+
+
+#region os
+import os
+print(                 __file__  )
+print(os.path.dirname (__file__) )
+print(os.path.basename(__file__) )
+#endregion os
