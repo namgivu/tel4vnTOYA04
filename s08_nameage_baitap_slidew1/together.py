7ca7139121d1242b0439c80e3519d065f9551bcf
namgivu <namgivu@gmail.com>
2023-12-12 12:51:39 +0000

    sync

--- s08_nameage_baitap_slidew1/together.py
+++ s08_nameage_baitap_slidew1/together.py
@@ -2,5 +2,19 @@
 Write a program that:
 ○ Input: name and year of birth.
 ○ Output: a string that “I am ... and I am ... years old.”
+                              name         age
 '''
-print('hi')
+
+# ref Phuong @ input name
+name = input ('Enter name: ')
+
+# ref Hieu kasumo @ input year
+year = input('Enter year: ')
+
+# ref Huynh kuzo @ get age
+from datetime import date
+today = date.today()
+age = today.year - int(year)
+
+# ref Mai Cuong @ print result
+print(f'I am {name} and I am {age} years old.')
