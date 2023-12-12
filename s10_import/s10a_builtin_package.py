6a02b4d00825a378e430be61b41bcf04d8957771
namgivu <namgivu@gmail.com>
2023-12-12 20:24:44 +0700

    builtin package @ random

--- s10_import/s10a_builtin_package.py
+++ s10_import/s10a_builtin_package.py
@@ -1,3 +1,4 @@
+#region demo datetime
 '''get today date'''
 #    package         module
 from datetime import date
@@ -12,3 +13,32 @@ d = datetime.now()
 print( datetime.strftime(d,                             '%Y%m%d_%H%M%S') )
 print( datetime.strftime(datetime(2000,1,22),           '%Y%m%d_%H%M%S') )
 print( datetime.strftime(datetime(2008,9,10, 11,12,13), '%Y%m%d_%H%M%S') )
+#endregion demo datetime
+
+print()
+
+
+#region demo random
+import random
+print( random.randint(0,1) )
+print( random.randrange(0,11, 2))  # same as randint() but can have :step param
+#                           , 2==step
+
+l = [1,22,333]
+print( random.choice(l) )
+
+#region codeforfun
+_=''
+_,                   l = _,[1,22,333]
+print( random.choice(l) )
+
+a = 1
+b = 22
+a,b = 1,22
+#endregion codeforfun
+
+print()
+l = [1,22,333]    ; print(f'before shuffle {l=}')
+random.shuffle(l) ; print(f'after  shuffle {l=}')
+
+#endregion demo random
