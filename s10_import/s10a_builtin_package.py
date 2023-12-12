3739e8f635ea513130a98bbab75ac872cbf3b754
namgivu <namgivu@gmail.com>
2023-12-12 20:07:08 +0700

    builtin package

---
+++ s10_import/s10a_builtin_package.py
@@ -0,0 +1,14 @@
+'''
+get today date
+'''
+#    package         module
+from datetime import date
+v = date.today() ; print(f'today is {v}')
+
+from datetime import datetime
+print(f'now is {datetime.now()}')
+
+d = datetime.now()
+print( datetime.strftime(d,                             '%Y%m%d_%H%M%S') )
+print( datetime.strftime(datetime(2000,1,22),           '%Y%m%d_%H%M%S') )
+print( datetime.strftime(datetime(2008,9,10, 11,12,13), '%Y%m%d_%H%M%S') )
