df921462eb744f9dc08917defe5b61d23d83828c
namgivu <namgivu@gmail.com>
2023-12-12 15:17:03 +0000

    sync doanso

--- s11_labw1_2__guessnum/together.py
+++ s11_labw1_2__guessnum/together.py
@@ -8,7 +8,7 @@ e. If user guess no more than 5 tries, they win the game.
 
 n=1
 1 2 3 4 5 6 7 8 9 10
-     lt 5 gt
+     lt 5 gtwhi
 """
 
 # ref Huong @  a. Using random package to generate a number  # only write import
@@ -20,17 +20,30 @@ secret_number = random.randint(1, 10)
 
 ### start @ let user guess 
 
-
-# ref Cuong @ nhap so nguoichoi doan  > Moi ban doan so? 
-so = input('Moi ban doan so? ')
-so = int(so)
-
-# ref Cuong @  c. If it lower than the secret_number: print that user’s number is lower.
-if so < secret_number:
-  print('So cua ban nho hon sobimat')
+'''
+ref Phuong @  b. User only has 5 tries to guess.
+'''
+i = 1
+while i<=5:
+  # ref Cuong @ nhap so nguoichoi doan  > Moi ban doan so? 
+  so = input('Moi ban doan so? ')
+  so = int(so)
   
-# ref Hieu kasumo @  d. If it higher than the secret_number: print that user’s number is higher.
-if int(so) > int(secret_number): 
-  print('So cua ban lon hon so bi mat')
+  # ref Cuong @  c. If it lower than the secret_number: print that user’s number is lower.
+  if so < secret_number:
+    print('So cua ban nho hon sobimat')
+    i = i + 1
+  
+  # ref Hieu kasumo @  d. If it higher than the secret_number: print that user’s number is higher.
+  if int(so) > int(secret_number): 
+    print('So cua ban lon hon so bi mat')
+    i = i + 1
+    
+  if so == secret_number:
+    print ('Chuc mung ban da doan dung!')
+    break
+
+if i > 5:
+  print ('Ban da thua cuoc')
 
 print(f'{secret_number=:>}')
