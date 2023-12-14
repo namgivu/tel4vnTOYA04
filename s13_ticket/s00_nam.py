fbb5e33b36c6627ffc8f543414af256e58ed95db
namgivu <namgivu@gmail.com>
2023-12-14 20:23:39 +0700

    baitap week1.2 ticket @ nam

--- s13_ticket/s00_nam.py
+++ s13_ticket/s00_nam.py
@@ -10,4 +10,24 @@ Write code that will do the following work:
 
 ○ Calculate the total money of those people’s tickets.
 """
-pass#todo
+def tien_ve(tuoi):
+  if                60<tuoi : return 50
+  if      18<=tuoi<=60      : return 100
+  if tuoi<18                : return 25
+
+n = input('nhap n: ')
+n = int(n)
+
+print()
+tongtien=0
+for i in range(1,n+1):
+  t = input(f'nhap tuoi nguoi thu {i=}: ')
+  t = int(t)
+
+  g = tien_ve(t)
+  tongtien += g
+
+  print(f'{tien_ve(t)=} -> {tongtien=}')
+  print()
+
+print(f'{tongtien=}')
