749701965460b1f4c20dde82ce1a36762d7e9981
namgivu <namgivu@gmail.com>
2023-12-14 20:55:50 +0700

    elif revisit

---
+++ s03_ifelse/s1_elif/s0_ifelse.py
@@ -0,0 +1,6 @@
+i = 122
+
+if i%2 == 0:
+  print('chan')
+else:
+  print('le')
---
+++ s03_ifelse/s1_elif/s1b_elif.py
@@ -0,0 +1,10 @@
+age = 22
+
+if age<16:
+  print('tre em')
+
+if 16<=age<=60:
+  print('nguoi lon')
+
+else:
+  print('lao thanh')
