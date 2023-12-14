444f015dc506e9ce6e0e99ac15da57357f83fce2
namgivu <namgivu@gmail.com>
2023-12-14 20:07:52 +0700

    baitap week1.2 ticket

---
+++ s13_ticket/s00_nam.py
@@ -0,0 +1,13 @@
+"""
+Write code that will do the following work:
+○ Input ”n” as number of people.
+○ Input ages of n people.
+
+○ Ticket price is:
+■ 100$ / 1 adult, 18 to 60 years old.
+■ 25$/ 1 children, which is younger than 18.
+■ 50$/1 person, which is older than 60.
+
+○ Calculate the total money of those people’s tickets.
+"""
+pass#todo
