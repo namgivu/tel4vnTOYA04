d66430409251b09b0d739a32f4cce4c47af3a23d
namgivu <namgivu@gmail.com>
2023-12-19 21:45:17 +0700

    str split() upper() title()

---
+++ s17_str/_2_split_hoten.py
@@ -0,0 +1,7 @@
+hoten='nguyen van duy'  # Duy most populoar vn name  ref https://g.co/bard/share/767290e8789f
+print( hoten )
+print( hoten.split(' ') )
+
+parts = hoten.split(' ')
+print(parts[2],         parts[1],         parts[0]         )
+print(parts[2].title(), parts[1].title(), parts[0].upper() )
