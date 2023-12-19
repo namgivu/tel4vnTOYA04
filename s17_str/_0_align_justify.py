539442b46d0a85a4129663abb63f3411c202de93
namgivu <namgivu@gmail.com>
2023-12-19 20:38:42 +0700

    str ljust() rjust() vs fstring align :.>

--- s17_str/_0_align_justify.py
+++ s17_str/_0_align_justify.py
@@ -1,23 +1,39 @@
 ### print ruler
-for _ in range(4):
-  for i in range(10): print(_,end='')
-print()
+def print_ruler():
+  for _ in range(4):
+    for i in range(10): print(_,end='')
+  print()
 
 
-for _ in range(4): print( str(_).ljust(10,'.'),end='')
-print()
+  for _ in range(4): print( str(_).ljust(10,'.'),end='')
+  print()
 
-for _ in range(4): print( str(_).rjust(10,'.'),end='')
-print()
+  for _ in range(4): print( str(_).rjust(10,'.'),end='')
+  print()
 
 
-for _ in range(4):
-  for i in range(10): print(i,end='')
-print()
+  for _ in range(4):
+    for i in range(10): print(i,end='')
+  print()
+
 
 ###
+print_ruler()
 
 s = 'python for devops'
 print(s)
 print(s.ljust(22,'.'),end='') ; print()
 print(s.rjust(22,'.'),end='') ; print()
+
+
+###
+print()
+print_ruler()
+
+print(f'{s}')
+
+print(f'{s: >22}')
+print(f'{s:.>22}')
+
+print(f'{s:<22}')
+print(f'{s:.<22}')
