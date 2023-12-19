fc12e9411a258eda51859f31271382edca1ca71e
namgivu <namgivu@gmail.com>
2023-12-19 20:41:14 +0700

    str ljust() rjust() @ printruler()

--- s17_str/_0_align_justify.py
+++ s17_str/_0_align_justify.py
@@ -11,6 +11,11 @@ def print_ruler():
   for _ in range(4): print( str(_).rjust(10,'.'),end='')
   print()
 
+  for _ in range(4):
+    print( str(_).ljust(10-1,'.'),end='')
+    print( str(_),                end='')
+
+  print()
 
   for _ in range(4):
     for i in range(10): print(i,end='')
