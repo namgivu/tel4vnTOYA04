27a23eb0a23f8245e1d50014a8b4913985c054a4
namgivu <namgivu@gmail.com>
2023-12-21 20:02:32 +0700

    noel tree reversed

--- s17_str/_1_noel_tree/together.py
+++ s17_str/_1_noel_tree/together.py
@@ -1,10 +1,14 @@
 
 def printnoeltree(total_height=6):
+  r = []
+
   digitlen = len(str(total_height))  # digitlen(total_height)
   for h in range(1, total_height + 1):
-    print(str(h).rjust(digitlen), end='')
-    #         h bug here for height>=10
-    #         h -> rjust(digitlen(total_height) )
+    s1 = str(h).rjust(digitlen)
+    #        h bug here for height>=10
+    #        h -> rjust(digitlen(total_height) )
+
+    print(s1, end='')
 
     # haft tree ontheleft
     # print other half ontheleft at height=h, starnum=h-1, rjust=total_height   # ref Duy  # ref Phuong
@@ -14,12 +18,20 @@ def printnoeltree(total_height=6):
     # print other half ontheleft at height=4, starnum=3,   rjust=6              # ref Hieu
     # print other half ontheleft at height=5, starnum=4,   rjust=6              # ref Hieu
     # print other half ontheleft at height=6, starnum=5,   rjust=6              # ref Hieu
-    print( ('*'*(h-1)).rjust(total_height), end='')
+    s2 = ('*'*(h-1)).rjust(total_height)
+    print(s2, end='')
 
     # haft tree ontheright
-    print('*'*h)
+    s3 = '*'*h
+    print(s3)
+    r.extend([s1+s2+s3])
 
-  print('*'.rjust(total_height+2) )
+  s = '*'.rjust(total_height+2)
+  print(s)
+  r.extend([s])
+
+  return r
+
   print()
   '''
   1 *          .*              .     *
@@ -37,3 +49,8 @@ printnoeltree(total_height=3)
 printnoeltree(total_height=6)
 printnoeltree(total_height=9)
 printnoeltree(total_height=12)
+
+r=printnoeltree(total_height=6)
+r.reverse()
+print()
+print('\n'.join(r) )
