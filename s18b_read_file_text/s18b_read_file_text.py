2974e2ac03260dadc2d97b81ef532f657a9e231a
namgivu <namgivu@gmail.com>
2023-12-21 21:04:10 +0700

    read file text

---
+++ s18b_read_file_text/s18b_read_file_text.py
@@ -0,0 +1,11 @@
+"""read file myfile.txt"""
+from pathlib import Path
+
+SH = Path(__file__).parent
+
+# f_path = f'{THIS_FILE_DIR}/myfile.txt'
+# f_path =      f'{THIS_DIR}/myfile.txt'
+f_path   =            f'{SH}/myfile.txt'  # SH aka SCRIPT_HOME
+
+s = Path(f_path).read_text()
+print(s)
