6927bba64768e56f8556b8b342c4d7ee0bb838fa
namgivu <namgivu@gmail.com>
2023-12-21 21:06:49 +0700

    SH / 'filename'

--- s18b_read_file_text/s18b_read_file_text.py
+++ s18b_read_file_text/s18b_read_file_text.py
@@ -5,7 +5,8 @@ SH = Path(__file__).parent
 
 # f_path = f'{THIS_FILE_DIR}/myfile.txt'
 # f_path =      f'{THIS_DIR}/myfile.txt'
-f_path   =            f'{SH}/myfile.txt'  # SH aka SCRIPT_HOME
+# f_path =            f'{SH}/myfile.txt'  # SH aka SCRIPT_HOME
+f_path   =               SH /'myfile.txt'  # SH aka SCRIPT_HOME
 
-s = Path(f_path).read_text()
+s = f_path.read_text()
 print(s)
