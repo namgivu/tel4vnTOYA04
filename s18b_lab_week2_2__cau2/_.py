38dc647eeb31c5cc9a0164dac9f9e4cbe6d31157
namgivu <namgivu@gmail.com>
2023-12-21 21:56:46 +0700

    lab week2.2 cau2 @ 4xx 5xx count

--- s18b_lab_week2_2__cau2/_.py
+++ s18b_lab_week2_2__cau2/_.py
@@ -34,8 +34,9 @@ logfile_p = SH/'sample.log'
 fc        = logfile_p.read_text()
 logline_list = fc.split('\n')
 
-#TODO split
+#region split
 def sol_split():
+  r = []
   for line in logline_list[:-4]:
     print(line)
     ip = line.split(' - - ')[0]
@@ -45,7 +46,39 @@ def sol_split():
 
     print(f'{ip=:>} {errcode=:>}')
     print()
+    r.append(
+      [ip,errcode]
+    )
+  print(r)
+
+  '''
+  TODO Duy 
+  [
+    ['66.249.65.159', '404'], 
+    ['172.25.65.3',   '200'], 
+    ['185.19.60.62',  '200'], 
+    ['47.29.201.179', '500'], ['66.249.65.159', '401'], ['47.29.201.179', '200'], ['47.29.201.179', '205'], ['66.249.65.159', '403'], ['47.29.201.179', '502'], ['66.249.65.159', '403'], ['47.29.201.179', '201']]
+  dem so luong 4xx
+  dem so luong 5xx
+  '''
+  def cach0_volong():
+    dem=0
+    for e in r:
+      if e[1].startswith('4') or e[1].startswith('5') :
+        print(e[1])
+        dem+=1
+    print(f'{dem=}')
+
+  print(
+    '4xx 5xx count: ',
+    len(
+      [ e for e in r  if e[1].startswith('4') or e[1].startswith('5') ]
+      # e for e ....  if filter1              or filter2
+    )
+  )
+
 sol_split()
+#endregion split
 
 #TODO regex
 
