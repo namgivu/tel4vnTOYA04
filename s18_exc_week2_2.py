5bd8c6f3a10b4e74420c0fa39c7cced019663ddf
namgivu <namgivu@gmail.com>
2023-12-21 13:25:43 +0000

    Phuong exc week2.2

---
+++ s18_exc_week2_2.py
@@ -0,0 +1,16 @@
+"""
+Giving url = "https://tel4vn.edu.vn/course/python-for-devops/"
+
+Split the string to get
+○ The protocol "https"
+○ 3 parts of domain ["tel4vn", "edu", "vn"]
+○ 2 parts of path ["course", "python-for-devops"]
+"""
+
+url = 'https://tel4vn.edu.vn/course/python-for-devops/'
+#   =        01             2      3        
+
+usp = url.split('/')
+print('ketqua', usp[0][:-1])  # https: 
+print(usp[2].split('.'))
+print(usp[3:-1])
