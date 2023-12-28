a61f7c3c7d9cc282eb91f0a05eb8efec5d4cf623
namgivu <namgivu@gmail.com>
2023-12-28 20:05:40 +0700

    requests.request(method='GET', url='https://api.upcitemdb.com...

--- s24_api/s231228_1955.py
+++ s24_api/s231228_1955.py
@@ -9,3 +9,14 @@ python -m virtualenv venv
 python -m pip install requests
 """
 import requests
+
+# call endpoint in slide 4.x
+res = requests.request(method='GET', url='https://api.upcitemdb.com/prod/trial/lookup?upc=190199267961')
+print(res)
+print(f'{res.status_code=}')
+print(f'{res.text=}')
+
+import json
+d = json.loads(res.text)
+print(d)
+print(json.dumps(d, indent=2)[:122] + '\n  ...\n}')
