22a51e727d679cea0fec9ec66168ba0bdc9ae4e2
namgivu <namgivu@gmail.com>
2023-12-28 20:47:50 +0700

    into method()

--- s24_api/s02_github_api.py
+++ s24_api/s02_github_api.py
@@ -5,11 +5,11 @@ eg latest tag   https://api.github.com/repos/OWNER/REPO/git/tags
 eg user's repo  https://api.github.com/users/tel4vn/repos
 '''
 
-u   = 'tel4vn'
-res = requests.request(method='GET', url=f'https://api.github.com/users/{u}/repos')
+def get_user_repo_on_github(u):
+  res = requests.request(method='GET', url=f'https://api.github.com/users/{u}/repos')
 
-d = res.json()
-d = [e.get('full_name') for e in d]
-print(d)
+  d = res.json()
+  d = [e.get('full_name') for e in d]
+  print(d)
 
-debug=122
+get_user_repo_on_github(u='tel4vn')
