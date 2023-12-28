561215803eb0b55fb7d6a24c485a45e0013ed09c
namgivu <namgivu@gmail.com>
2023-12-28 19:59:33 +0700

    pip install requests

---
+++ s24_api/s231228_1955.py
@@ -0,0 +1,11 @@
+"""
+# project-wise folder-wise
+python -m pip install --upgrade pip virtualenv
+python -m virtualenv venv
+./venv/Scripts/python.exe -m pip install requests
+
+
+# system-wise
+python -m pip install requests
+"""
+import requests
