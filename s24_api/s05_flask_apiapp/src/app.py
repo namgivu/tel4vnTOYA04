d55310f04f13210af68e45b910f47868919057db
namgivu <namgivu@gmail.com>
2024-01-02 21:14:11 +0700

    flask apiapp 0th

---
+++ s24_api/s05_flask_apiapp/src/app.py
@@ -0,0 +1,10 @@
+"""
+python -V
+python -m pip install --upgrade pip virtualenv
+python -m virtualenv venv
+
+./venv/bin/python -m pip freeze
+./venv/bin/python -m pip install flask
+./venv/bin/python -m pip freeze
+# win /Scripts/python.exe
+"""
