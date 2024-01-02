37d78fc5a21ed7b184e65de17c30b5c4d489067e
namgivu <namgivu@gmail.com>
2024-01-02 21:45:56 +0700

    GET /github_latest_release w/ test by requests

--- s24_api/s05_flask_apiapp/tests/test_by_requests.py
+++ s24_api/s05_flask_apiapp/tests/test_by_requests.py
@@ -1,5 +1,12 @@
 import requests
 
-res = requests.request('GET', 'http://127.0.0.1:5000')
-print(res)
-print(res.json() )
+def test_index():
+  res = requests.request('GET', 'http://127.0.0.1:5000')
+  print(res)
+  print(res.json() )
+
+
+def test_github_latest_release():
+  res = requests.request('GET', 'http://127.0.0.1:5000/github_latest_release')
+  print(res)
+  print(res.json() )
