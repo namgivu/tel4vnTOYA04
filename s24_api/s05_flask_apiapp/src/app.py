3490382beb3fd38d53bb3496b18fdab9b2eeb162
namgivu <namgivu@gmail.com>
2024-01-02 21:20:31 +0700

    flask app 0th

--- s24_api/s05_flask_apiapp/src/app.py
+++ s24_api/s05_flask_apiapp/src/app.py
@@ -8,3 +8,12 @@ python -m virtualenv venv
 ./venv/bin/python -m pip freeze
 # win /Scripts/python.exe
 """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+  #  .run                        host to run as dockercontainer lateron
+  app.run(debug=True, port=5000, host='0.0.0.0')
+  #  .run debug=True autoreload code if changed
