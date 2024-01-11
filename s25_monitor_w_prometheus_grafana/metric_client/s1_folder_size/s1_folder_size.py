1743400919f8d19d077bee0e835f10fc72b86511
namgivu <namgivu@gmail.com>
2024-01-11 22:04:58 +0700

    monitor foldersize @ serve 8080 soas :prometheus container can scape

--- s25_monitor_w_prometheus_grafana/metric_client/s1_folder_size/s1_folder_size.py
+++ s25_monitor_w_prometheus_grafana/metric_client/s1_folder_size/s1_folder_size.py
@@ -7,14 +7,18 @@ in short, need a monitor of docker cache 's occupied disk size
 ie monitor a folder size
 
 DONE get foldersize fr forlder_path  ref https://stackoverflow.com/a/25574638
-TODO send foldersize plot to prometheus --> later have grafana draw it
+DONE send foldersize plot to prometheus --> later have grafana draw it
 """
 import subprocess
+from prometheus_client import start_http_server
+
 
 def get_folder_size(path):
   """disk usage in human readable format (e.g. '2,1GB') ref https://stackoverflow.com/a/25574638"""
   return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
 
 if __name__ == '__main__':
-  p='/var/lib/docker' ; print( f'{get_folder_size(p)} {p}' )
-  p='/var/cache'      ; print( f'{get_folder_size(p)} {p}' )
+  # p='/var/lib/docker' ; print( f'{get_folder_size(p)} {p}' )
+  # p='/var/cache'      ; print( f'{get_folder_size(p)} {p}' )
+
+  start_http_server(8080)  # start a prometheus metricclient; it's a httpserver app; aka 'prometheus metric-client httpserver' app
