b0f9503b01a0090af561b4dd93fe0737e4346fdb
namgivu <namgivu@gmail.com>
2024-01-11 22:02:43 +0700

    monitor foldersize 0th

---
+++ s25_monitor_w_prometheus_grafana/metric_client/s1_folder_size/s1_folder_size.py
@@ -0,0 +1,20 @@
+"""
+company project use docker image to run on gcp cloudrun
+image built from a computeengine server --> docker cache keep growing big thru time
+eg after 3 months, it grows to 60Gb --> making the server stop working --> need a docker cache clean/prune
+
+in short, need a monitor of docker cache 's occupied disk size
+ie monitor a folder size
+
+DONE get foldersize fr forlder_path  ref https://stackoverflow.com/a/25574638
+TODO send foldersize plot to prometheus --> later have grafana draw it
+"""
+import subprocess
+
+def get_folder_size(path):
+  """disk usage in human readable format (e.g. '2,1GB') ref https://stackoverflow.com/a/25574638"""
+  return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
+
+if __name__ == '__main__':
+  p='/var/lib/docker' ; print( f'{get_folder_size(p)} {p}' )
+  p='/var/cache'      ; print( f'{get_folder_size(p)} {p}' )
---
+++ s25_monitor_w_prometheus_grafana/metric_client/s1_folder_size/s1_folder_size.sh
@@ -0,0 +1,2 @@
+SH=$(cd `dirname ${BASH_SOURCE:-$0}` && pwd)
+sudo "$SH/../venv/bin/python"  "$SH/s1_folder_size.py"
